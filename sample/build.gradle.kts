9438b80590ea3cc93af2409cd0c6f08c17dc7a9b
mijeong.ko <mijeong.ko@hunet.co.kr>
2024-08-29 15:30:36 +0900

    * v.1.0.4

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -57,7 +57,7 @@ android {
         applicationId = "com.mygomii"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
     }
     packaging {
